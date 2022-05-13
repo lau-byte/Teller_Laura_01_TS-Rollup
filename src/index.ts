f40cc4b7861f06d9e40eb1d8f67a2e3820609ef5
lau-byte <lau.teller.21@lehre.mosbach.dhbw.de>
2022-05-13 23:54:56 +0200

    Ergänzungen/CSS Finalisierung
    Ergänzungen und Aufräumen des Codes und Anpassung des CSS an das Spielthema

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-import { helloWorld, Beispiel } from "./myModule";
-import { alertMe } from "./myOtherModule";
 import { startMemoryGame } from "./app";
 
-console.log(helloWorld);
-customElements.define("my-beispiel", Beispiel);
-
-alertMe();
 startMemoryGame();
-const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
-
-const myInputValueAlternate = document.querySelector(
-  "#myInput"
-) as HTMLInputElement;
-
-document
-  .querySelector<HTMLInputElement>("#myInput")
-  ?.addEventListener("focus", doSmth);
-
-function doSmth(e: UIEvent) {
-  const val = e.target as HTMLInputElement;
-  console.log(e, val.value);
-}
